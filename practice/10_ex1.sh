76bd54bb3605bc3904958aa080f4557b7f1e8c62
gregjanowiak <greg@blend.com>
2018-06-04 21:14:02 -0700

    adding ex 10 file

---
+++ practice/10_ex1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+#script that exits on error and displays commands as they execute
+
+#ls command that succeeds
+ls
+
+#ls command that fails
+ls /not/here
+
+#ls command that succeeds
+ls
